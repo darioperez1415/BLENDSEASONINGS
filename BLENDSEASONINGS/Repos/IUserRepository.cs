50c1be3975a77ec69399ccf793dfb1ec8fbddb3e
darioperez1415 <darioperez1415@gmail.com>
2022-06-09 18:31:08 -0500

    Added user data

--- BLENDSEASONINGS/Repos/IUserRepository.cs
+++ BLENDSEASONINGS/Repos/IUserRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace BLENDSEASONINGS.Repos
+﻿using BLENDSEASONINGS.Models;
+
+namespace BLENDSEASONINGS.Repos
 {
     public interface IUserRepository
     {
+        List<User> GetUsers();
+        User GetUserById(string id);
+        void AddUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(string id);
     }
 }
