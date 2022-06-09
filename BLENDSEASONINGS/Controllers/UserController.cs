59ede5240da9359c3fe29d2bf87d17261dcc160e
darioperez1415 <darioperez1415@gmail.com>
2022-06-09 18:21:51 -0500

    Added user info

--- BLENDSEASONINGS/Controllers/UserController.cs
+++ BLENDSEASONINGS/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using BLENDSEASONINGS.Repos;
+using BLENDSEASONINGS.Models;
 
 namespace BLENDSEASONINGS.Controllers
 {
@@ -7,5 +9,48 @@ namespace BLENDSEASONINGS.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserRepository _userRepo;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepo = userRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            List<User> users = _userRepo.GetUsers();
+            if (users == null) return NotFound();
+            return Ok(users);
+        }
+
+
+        // GET: UserController/5
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(string id)
+        {
+            User user = _userRepo.GetUserById(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User newUser)
+        {
+            if (newUser == null)
+            {
+                return NotFound();
+
+            }
+            else
+            {
+                _userRepo.AddUser(newUser);
+                return Ok(newUser);
+            }
+        }
     }
 }
---
+++ SQLQuery1.sql
@@ -0,0 +1,23 @@
+﻿Select * from Ingredient
+
+Select * from Blend
+
+select * from Spice
+
+Select Ingredient.id, Ingredient.blendId, Ingredient.spiceId,Spice.id, Spice.name, Spice.weight, Spice.price, Spice.imageUrl
+From Ingredient
+Right join Spice
+ON Ingredient.id = Spice.id
+Union
+Select Blend.id, Blend.name,Ingredient.id, Ingredient.blendId,Ingredient.spiceId
+From Blend
+Left Join Ingredient
+ON Blend.id = Ingredient.blendId
+
+
+Select Blend.id, Blend.name,Blend.weight
+From Blend
+Inner Join Ingredient
+ON Blend.id = Ingredient.id
+Inner Join Spice 
+On Ingredient.blendId = Ingredient.id
