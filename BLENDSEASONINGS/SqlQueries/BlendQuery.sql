055bc67bb03729437c95dda332c9912cef6fa664
darioperez1415 <darioperez1415@gmail.com>
2022-06-11 13:25:32 -0500

    Added Blend Query

---
+++ BLENDSEASONINGS/SqlQueries/BlendQuery.sql
@@ -0,0 +1,30 @@
+﻿SELECT * FROM Blend
+
+SELECT * FROM Ingredient
+
+SELECT * FROM Spice
+
+SELECT * FROM [Order]
+
+SELECT * FROM [User]
+
+-- Order by Blend ID
+-- Order by Blend
+-- Track ingredients
+-- 
+
+SELECT Blend.name, Blend.weight, Blend.id, Ingredient.blendId, Ingredient.spiceId, 
+Spice.id, Spice.imageUrl, Spice.name, Spice.price, Spice.weight
+FROM Blend
+INNER JOIN Ingredient
+On Blend.id = Ingredient.blendId
+INNER JOIN Spice
+ON Ingredient.spiceId = Spice.id
+Order by Blend.id
+
+
+SELECT Ingredient.blendId, Ingredient.spiceId,
+Spice.id, Spice.imageUrl, Spice.name, Spice.price, Spice.weight
+FROM Ingredient
+INNER JOIN Spice
+On Ingredient.spiceId = Spice.id
